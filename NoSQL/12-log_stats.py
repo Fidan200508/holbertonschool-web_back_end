bd85a580ee5654fdaab592b79d30549dcb2d9fc0
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-20 18:47:04 +0000

    Fix log stats script and make it executable

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,8 +2,6 @@
 """Module for logging stats"""
 from pymongo import MongoClient
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
 if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
@@ -14,10 +12,10 @@ if __name__ == "__main__":
 
     # methods
     print("Methods:")
-    for method in METHODS:
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
     # status check
-    status_check = collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
+    status = collection.count_documents({"path": "/status"})
+    print(f"{status} status check")
